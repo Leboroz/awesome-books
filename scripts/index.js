bdfbf1ca11846d8e53617c1e721f8dfc97101721
leonardo <albornozleo95@gmail.com>
2022-01-03 12:02:19 -0400

    Create book class and add book function

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+
+ const books = [];
+
+ const Book = (title, author) => {
+  this.title = title;
+  this.author = author;
+ }
+
+ const createAndAddNewBook = (title, author, collection) => {
+  collection.push(new Book(title, author));
+ }
+
+ const removeBook = (reference, collection) => {
+  collection.filter(book => book.title !== reference || book.author !== reference);
+ }
+
+ const bookSection = document.getElementById('book_section');
+ 
+}
