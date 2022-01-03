984180e7c05c991ec4df336e7708cc9b77cae202
leonardo <albornozleo95@gmail.com>
2022-01-03 12:25:58 -0400

    Add event listener to add_button

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,37 @@ window.onload = () => {
  const books = [];
 
  const Book = (title, author) => {
+  const element = document.createElement('div');
+  element.className = 'card'
+  element.innerHTML = 
+  `<h2 class="card-title">${title}</h2>
+   <h3 class="card-author">${author}</h3>
+   <button type="button" class="btn" onclick="removeHandler()">Remove</button>
+  `
   this.title = title;
   this.author = author;
+  this.element = element;
  }
 
  const createAndAddNewBook = (title, author, collection) => {
-  collection.push(new Book(title, author));
+  return collection.length < collection.push(new Book(title, author));
  }
 
  const removeBook = (reference, collection) => {
   collection.filter(book => book.title !== reference || book.author !== reference);
  }
+
+ document
+ .getElementById('add_button')
+ .addEventListener('click', () => {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  if(createAndAddNewBook(title, author, books)){
+   const bookSection = document.getElementById('book_section');
+   bookSection.innerHTML = '';
+   bookSection.append(...books.map(book => book.element));
+  }
+ });
+
  
 }
