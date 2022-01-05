429555d4cb3b333f5212e69a15ff3d223a08883a
leonardo <albornozleo95@gmail.com>
2022-01-05 16:34:42 -0400

    fix linters

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,7 @@ window.onload = () => {
   const date = document.getElementById('date');
   setInterval(() => {
     date.innerHTML = new Date().toUTCString();
-  }, 1000)
+  }, 1000);
   const removeHandler = (e) => {
     const card = e.target.parentNode;
     const title = card.children[0].innerHTML;
@@ -26,53 +26,59 @@ window.onload = () => {
   };
 
   if (localStorage.getItem('books') !== null) {
-    bookList.list = JSON.parse(localStorage.getItem('books')).map(book => new Book(book.title, book.author));
+    bookList.list = JSON.parse(localStorage.getItem('books')).map((book) => new Book(book.title, book.author));
     bookSection
       .append(...bookList.list.map(
-        (book, index) => book.createCard(index, removeHandler)
+        (book, index) => book.createCard(index, removeHandler),
       ));
     if (bookList.list.length !== 0) {
       bookSection.setAttribute('data-visible', 'true');
     }
   }
-  
+
   document
     .getElementById('form')
     .addEventListener('submit', (e) => {
       const title = document.getElementById('title').value;
       const author = document.getElementById('author').value;
       const book = bookList.createAndAddNewBook(title, author);
-      console.log(bookList[bookList.list.length - 1])
       if (title && author && book) {
-        bookSection.appendChild(bookList.list[bookList.list.length - 1].createCard(bookList.list.length - 1, removeHandler));
+        bookSection
+          .appendChild(
+            bookList.list[bookList.list.length - 1].createCard(
+              bookList.list.length - 1, removeHandler,
+            ),
+          );
         bookSection.setAttribute('data-visible', 'true');
         updateLocalStorage();
         e.clear();
-        e.preventDefault()
+        e.preventDefault();
       }
     });
 
   sections.forEach((section, index) => {
     const sectionWidth = section.getBoundingClientRect().width;
-    section.style.left = sectionWidth * index + 'px'
+    section.style.left = `${sectionWidth * index}px`;
   });
 
   const moveToSection = (container, currentSection, targetSection) => {
-      container.style.transform = 'translateX(-' + targetSection.style.left + ')'
-      currentSection.classList.remove('current-section');
-      targetSection.classList.add('current-section');
-  }
-  
-  navLinks.addEventListener('click', e => {
+    container.style.transform = `translateX(-${targetSection.style.left})`;
+    currentSection.classList.remove('current-section');
+    targetSection.classList.add('current-section');
+  };
+
+  navLinks.addEventListener('click', (e) => {
     const targetLink = e.target.closest('li');
-    if(!targetLink) return;
+    if (!targetLink) return;
     const currentSection = tracks.querySelector('.current-section');
-    const targetIndex = Array.from(e.target.parentNode.children).findIndex(link => link === targetLink)
+    const targetIndex = Array
+      .from(e.target.parentNode.children)
+      .findIndex((link) => link === targetLink);
     const targetSection = sections[targetIndex];
 
     navLinks.querySelector('.nav-link__active').classList.remove('nav-link__active');
     targetLink.classList.add('nav-link__active');
 
     moveToSection(tracks, currentSection, targetSection);
-  })
+  });
 };
